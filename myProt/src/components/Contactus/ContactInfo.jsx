4e145c6d617b67bbb214cc6b57f2ea24b610852f
sheikhhaseeb937 <sheikhhaseeb937@gmail.com>
2025-09-09 08:40:15 -0700

    2nd

--- myProt/src/components/Contactus/ContactInfo.jsx
+++ myProt/src/components/Contactus/ContactInfo.jsx
@@ -1,14 +1,14 @@
 import { HiOutlineMail } from "react-icons/hi";
 import { FiPhone } from "react-icons/fi";
 import { IoLocationOutline } from "react-icons/io5";
-import SingleInfo from "./SingleInfo";
+import SingleInf from "./Singleinfo";
 
 const ContactInfo = () => {
   return (
     <div className="flex flex-col gap-4 text-white">
-      <SingleInfo  text="sheikhaseeb937@gmail.com" Image={HiOutlineMail} />
-      <SingleInfo  text="+92 3150006458" Image={FiPhone} />
-      <SingleInfo text="Karachi Pakistan" Image={IoLocationOutline} />
+      <SingleInf  text="sheikhaseeb937@gmail.com" Image={HiOutlineMail} />
+      <SingleInf text="+92 3150006458" Image={FiPhone} />
+      <SingleInf text="Karachi Pakistan" Image={IoLocationOutline} />
     </div>
   );
 };
